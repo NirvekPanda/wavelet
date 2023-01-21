0d2dee8615aea726807fa978133d3cc17b9bdd59
Nirvek <nirvekpandey@gmail.com>
2023-01-21 14:51:07 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,14 +1,42 @@
-public class SearchEngine {
-/* 
+import java.io.IOException;
+import java.net.URI;
 
-/add?s=anewstringtoadd
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    String text = "Hello, world!";
 
-/add?s=pineapple
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Name is nirvek and Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Fruit is %d", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
 
-/add?s=apple
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
 
-/search?s=app
-(would return pineapple and apple)
+        int port = Integer.parseInt(args[0]);
 
-*/    
+        Server.start(port, new Handler());
+    }
 }
